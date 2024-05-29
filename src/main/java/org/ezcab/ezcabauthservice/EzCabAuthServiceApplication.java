4ca00c256511f98892359efc51a1b8ff6a8cbdf7
Manish Prasad <prasad.bmanish999@gmail.com>
2024-05-29 19:09:03 +0530

    first commit

---
+++ src/main/java/org/ezcab/ezcabauthservice/EzCabAuthServiceApplication.java
@@ -0,0 +1,15 @@
+package org.ezcab.ezcabauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzCabAuthServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EzCabAuthServiceApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/org/ezcab/ezcabauthservice/EzCabAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ezcab.ezcabauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzCabAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
